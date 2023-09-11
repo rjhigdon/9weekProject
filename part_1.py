0b02b109419e36857781c1f7a58b4b1deea05a00
rjhigdon <rjocunts@protonmail.com>
2023-09-10 22:36:02 -0700

    unit 1 project

--- part_1.py
+++ part_1.py
@@ -17,23 +17,23 @@ sentence1 = book_name + " is written by " + author + "."
 sentence2 = f"the author, {author}, wrote the book {book_name}"
 # sentence2 = f"Author, {author}, wrote the book {book_name}."
 sentence3 = "The book {} was written by {}".format(book_name, author)
-
+print(sentence1, sentence2, sentence3)
 # Step 4 - Integers
 # Now let's practice Integers. Replace the "None" below with the publication date of your book.
 publication_year = 1667
-# publication_year = 1965
+
 
 
 # Step 5 - Floats
 # Now estimate what the retail value of your book would be at a store and replace the "None" below with a float value of the price. (Doesn't have to be accurate.)
 book_price = 16.67
-# book_price = 17.99
+
 
 
 # Step 6 - Booleans
 # Now we will practice with booleans. Replace the "is_awesome" variable with a boolean. True if your book is awesome, False if your book is not awesome.
 is_awesome = True
-# is_awesome = True
+
 
 
 # Step 7 - print and type functions
--- part_2.py
+++ part_2.py
@@ -1,70 +1,51 @@
 ### Step 1 - Lists ###
 
 # Fill in this list with several authors you are a fan of. At least 7 or 8 should do.
-my_authors = ["John Milton", "JK Rownling", "Earnest Hemingway", "George Orwell", "Aldous Huxley"]
+my_authors = ["John Milton", "JK Rownling", "Earnest Hemingway", "George Orwell", "Aldous Huxley", "CS Lewis", "JRR Tolkein"]
 
-# Now let's add a new author to the end with the .append() method. Type your code below.
+# Now let's add a new author to the end with the .append() method. Type your code below. Code here
 
-# Code here
-# Example: my_authors.append("H.G. Wells")
 my_authors.append("Suzanne Collins")
 
-# Now let's remove an element in the list
+# Now let's remove an element in the list. Code here
 
-# Code here
-# Example: my_authors.remove("H.G. Wells")
 my_authors.remove("Suzanne Collins")
 
-# Now access an element by it's index. (Remember it indexes start at 0.)
-
-# Code here
-# Example: my_authors[2]
+# Now access an element by it's index. (Remember it indexes start at 0. Code here
 
 my_authors[2]
 
-# Now slice the list.
+# Now slice the list.Code here
 
-# Code here
-# Example: my_authors[1:4]
 my_authors[1:2]
 
 ### Step 2 - Tuples ###
 
-# Create your tuple below. Assign it to a variable name you can reference later in the exercise.
+# Create your tuple below. Assign it to a variable name you can reference later in the exercise.Code Here
 
-# Code here
-# Example: my_author_tuple = ("F. Scott Fitzgerald", "J.K. Rowling", "Ernest Hemingway", "Emily Dickenson", "George Orwell", "Ray Bradbury")
-
-my_author_tuple = ("John Milton", "JK Rownling", "Earnest Hemingway", "George Orwell", "Aldous Huxley")
+my_author_tuple = ("John Milton", "JK Rownling", "Earnest Hemingway", "George Orwell", "Aldous Huxley", "CS Lewis", "JRR Tolkein")
 
 ### Step 3 - Sets ###
 
 # Create a set with your authors.
 
 # Code here
-# Example: my_author_set = {"F. Scott Fitzgerald", "J.K. Rowling", "Ernest Hemingway", "Emily Dickenson", "George Orwell", "Ray Bradbury"}
-my_author_set = {"John Milton", "JK Rownling", "Earnest Hemingway", "George Orwell", "Aldous Huxley"}
+my_author_set = {"John Milton", "JK Rownling", "Earnest Hemingway", "George Orwell", "Aldous Huxley", "CS Lewis", "JRR Tolkein"}
 
 # Add a new author to your set.
 
 # Code here
-# Example: my_author_set.add("J.R.R. Tolkien")
+
 my_author_set.add("Suzanne Collins")
 
 # Try adding the same author again, and be sure to print your set.
 
-# Code here
-# Example: my_author_set.add("J.R.R. Tolkien")
 my_author_set.add("Suzanne Collins")
-print(my_author_set)
-
 
 ### Step 4 - For Loops ###
 
 # Create a for-loop for each of the data-structures above.
-
 # Code here
-# Example:
 
 for book in my_authors:
     print(book)
